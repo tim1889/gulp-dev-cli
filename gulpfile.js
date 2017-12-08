704fa093291928cb3b0b426d32662f97d13db018
unknown <yaha@UOKO.local>
2017-12-08 17:37:29 +0800

    添加babel转换

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,8 @@ cache = require('gulp-cache')
 rev = require('gulp-rev'),//添加时间戳
 revCollector = require('gulp-rev-collector'),
 watch = require('gulp-watch'),
-connect = require('gulp-connect');
+connect = require('gulp-connect'),
+babel = require('gulp-babel');
 
 var config = {
 input: {
@@ -30,72 +31,83 @@ output: {
 }
 
 gulp.task('html', function () {
-  return gulp.src(config.input.html)
-    .pipe(gulp.dest('dist'))
-    .pipe(connect.reload());
+return gulp.src(config.input.html)
+  .pipe(gulp.dest('dist'))
+  .pipe(connect.reload());
 
 });
 
-gulp.task('js', function (done) {
+gulp.task('js', function () {
 return gulp.src(config.input.js)
-  .pipe(uglify())
-  .pipe(rev())
-  .pipe(gulp.dest(config.output.js))
-  .pipe(rev.manifest())
-  .pipe(gulp.dest('./rev/js'))
-  .pipe(connect.reload());
+.pipe(babel({
+  presets: ['es2015','es2016','es2017'],
+  plugins: [["transform-runtime", {
+    "polyfill": false,
+    "regenerator": true
+  }]]
+}))
+.pipe(uglify())
+.pipe(rev())
+.pipe(gulp.dest(config.output.js))
+.pipe(rev.manifest())
+.pipe(gulp.dest('./rev/js'))
+.pipe(connect.reload())
+.on('error', function (err) {
+  console.log(err.stack);
+  this.emit('end');
+});
 });
 
 gulp.task('css', function () {
 return gulp.src(config.input.css)
-  .pipe(minifyCss())
-  .pipe(rev())    
-  .pipe(gulp.dest(config.output.css))
-  .pipe(rev.manifest())
-  .pipe(gulp.dest('./rev/css'))
-  .pipe(connect.reload());
+.pipe(minifyCss())
+.pipe(rev())    
+.pipe(gulp.dest(config.output.css))
+.pipe(rev.manifest())
+.pipe(gulp.dest('./rev/css'))
+.pipe(connect.reload());
 });
 
 gulp.task('less', function () {
-  return gulp.src(config.input.css)
-      .pipe(less())
-      .pipe(minifyCss())
-      .pipe(rev())     
-      .pipe(gulp.dest(config.output.css))    
-      .pipe(gulp.dest('./rev/css'))
-      .pipe(connect.reload());
+return gulp.src(config.input.css)
+    .pipe(less())
+    .pipe(minifyCss())
+    .pipe(rev())     
+    .pipe(gulp.dest(config.output.css))    
+    .pipe(gulp.dest('./rev/css'))
+    .pipe(connect.reload());
 });
 
 gulp.task('img', function () {
 return gulp.src(config.input.img)
-    .pipe(cache(imagemin({
-        progressive: true,
-        svgoPlugins: [{removeViewBox: false}],
-        use: [pngquant()]
-    })))
-    .pipe(gulp.dest('dist/img'))
-    .pipe(connect.reload());
+  .pipe(cache(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+  })))
+  .pipe(gulp.dest('dist/img'))
+  .pipe(connect.reload());
 });
 
 gulp.task('rev', function() {
 var options = {
-  removeComments: true,
-  collapseWhitespace: true,
-  collapseBooleanAttributes: true,
-  removeEmptyAttributes: true,
-  removeScriptTypeAttributes: true,
-  removeStyleLinkTypeAttributes: true,
-  minifyJS: true,
-  minifyCSS: true
+removeComments: true,
+collapseWhitespace: true,
+collapseBooleanAttributes: true,
+removeEmptyAttributes: true,
+removeScriptTypeAttributes: true,
+removeStyleLinkTypeAttributes: true,
+minifyJS: true,
+minifyCSS: true
 };
 gulp.src(['./rev/**/*.json', 'src/*.html'])   
 .pipe( revCollector({
-  replaceReved: true,
-  dirReplacements: {
-      'css': 'css/',
-      'js': 'js/'
-    }
-  }) 
+replaceReved: true,
+dirReplacements: {
+    'css': 'css/',
+    'js': 'js/'
+  }
+}) 
 )              
 .pipe(revCollector())         
 .pipe(htmlmin(options))  
@@ -104,7 +116,7 @@ gulp.src(['./rev/**/*.json', 'src/*.html'])
 
 gulp.task('connect',function(){
 connect.server({
-  livereload: true
+livereload: true
 });
 })
 
@@ -112,11 +124,12 @@ gulp.task('watch',function(){
 gulp.watch(config.input.html, ['html']);
 gulp.watch(config.input.js, ['js']);
 gulp.watch(config.input.css, ['css']);
+gulp.watch(config.input.css, ['less']);
 })
 
 gulp.task('default',function(){
   /**压缩打包 */
-// gulp.run('img', 'js', 'css', 'rev');
-/**自动刷新 */
-gulp.run('img', 'js', 'css','watch', 'connect');
+  // gulp.run('img', 'js', 'css', 'html', 'rev');
+  /**自动刷新 */
+  gulp.run('watch', 'connect');
 });
